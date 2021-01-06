0333cfbb741a429e2d3977500d26263b44190cae
morty <morty@morty.com>
2021-01-06 07:06:19 +0530

    MAJOR UPDATE

--- install.sh
+++ install.sh
@@ -1,195 +1,287 @@
 #!/bin/bash
-
 #------------------------------#
 ## Resources
-
-# https://github.com/swisskyrepo/PayloadsAllTheThings
-# https://github.com/danielmiessler/SecLists
-# https://github.com/projectdiscovery
-# https://github.com/hahwul/WebHackersWeapons
-# https://github.com/sehno/Bug-bounty/blob/master/bugbounty_checklist.md
-# https://github.com/tomnomnom
-# https://github.com/internetwache/CT_subdomains
-# https://github.com/hahwul/s3reverse
 # https://github.com/dwisiswant0/go-dork
-# https://github.com/lc/gau
-
+# https://github.com/hahwul/WebHackersWeapons
+# https://github.com/internetwache/CT_subdomains
+# https://github.com/sehno/Bug-bounty/blob/master/bugbounty_checklist.md
 #------------------------------#
-## Basic Utils
 
-# Installing base
-sudo apt update && sudo apt full-upgrade
-sudo apt install git vim nano tmux rlwrap \
+base_i(){
+    sudo apt update && sudo apt full-upgrade -y
+    sudo apt install git vim nano tmux rlwrap \
     jq python3 python3-pip net-tools \
-    nmap gawk curl wget fping toilet masscan chromium-browser -y
+    nmap gawk curl wget fping toilet whatweb masscan chromium-browser gcc make libpcap-dev -y
+    python3 -m pip install --user virtualenv
 
-# Installing virtualenv
-python3 -m pip install --user virtualenv
+    git clone https://github.com/mrjoker05/DotFiles
+    cp DotFiles/tmux.conf ~/.tmux.conf
+    cp DotFiles/vimrc ~/.vimrc
+    sudo mv DotFiles /opt
+    
+}
 
-# Installing go
-cd /tmp
-wget "https://golang.org/dl/go1.15.5.linux-amd64.tar.gz"
-sudo tar -C /usr/local -xzf go1.15.5.linux-amd64.tar.gz
-mkdir -p ~/go_projects/{bin,src,pkg}
-export GOPATH="$HOME/go_projects"
-export GOBIN="$GOPATH/bin"
-export PATH=$PATH:/usr/local/go/bin
-echo "export PATH=$PATH:/usr/local/go/bin:$GOBIN" >> ~/.bashrc
-echo 'export GOBIN="$GOPATH/bin"' >> ~/.bashrc
-echo 'export GOPATH="$HOME/go_projects"' >> ~/.bashrc
-rm -rf *.tar.gz
+go_i(){
+    cd /tmp
+    wget "https://golang.org/dl/go1.15.5.linux-amd64.tar.gz"
+    sudo tar -C /usr/local -xzf go1.15.5.linux-amd64.tar.gz
+    mkdir -p ~/go_projects/{bin,src,pkg}
+    export GOPATH="$HOME/go_projects"
+    export GOBIN="$GOPATH/bin"
+    export PATH=$PATH:/usr/local/go/bin
+    echo "export PATH=$PATH:/usr/local/go/bin:$GOBIN" >> ~/.bashrc
+    echo 'export GOBIN="$GOPATH/bin"' >> ~/.bashrc
+    echo 'export GOPATH="$HOME/go_projects"' >> ~/.bashrc
+    rm -rf *.tar.gz
+}
 
-#------------------------------#
+gotools_i(){
+    GO111MODULE=on go get -u -v github.com/projectdiscovery/naabu/v2/cmd/naabu
+    go get github.com/tomnomnom/waybackurls
+    GO111MODULE=on go get -u -v github.com/lc/gau
+    go get -u github.com/hahwul/dalfox
+    go get -u github.com/ffuf/ffuf
+    GO111MODULE=on go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+    GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+    GO111MODULE=on go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns
+    GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
+}
 
-## Tools
+masscan_i(){
+    cd /tmp
+    git clone https://github.com/robertdavidgraham/masscan
+    cd masscan
+    make -j
+    sudo mv bin/masscan /opt    
+}
 
-# ffuf
-go get -u github.com/ffuf/ffuf
+amass_i(){
+    # cd /tmp
+    # wget "https://github.com/OWASP/Amass/releases/download/v3.10.5/amass_linux_amd64.zip"
+    # unzip amass*
+    # sudo mv amass* /opt/amass
+    export GO111MODULE=on
+    go get -v -u github.com/OWASP/Amass/v3/...
+}
 
-# masscan
-cd /tmp
-sudo apt-get install git gcc make libpcap-dev
-git clone https://github.com/robertdavidgraham/masscan
-cd masscan
-make -j
-sudo mv bin/masscan /opt
+massdns_i(){
+    cd /tmp
+    git clone https://github.com/blechschmidt/massdns
+    cd massdns
+    make
+    sudo cp bin/massdns /opt
+}
 
-# shuffledns
-GO111MODULE=on go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns
+param_i(){
 
-# amass
-cd /tmp
-wget "https://github.com/OWASP/Amass/releases/download/v3.10.5/amass_linux_amd64.zip"
-unzip amass*
-sudo mv amass* /opt
-echo "export PATH=$PATH:/opt/amass_linux_amd64" >> ~/.bashrc
-echo "export PATH=$PATH:/opt/amass_linux_amd64" >> ~/.zshrc
+    if [[ $# -ne 0]]
+    then
+        sudo rm -rf /opt/ParamSpider
+    fi
 
-# subfinder
-GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+    git clone https://github.com/devanshbatham/ParamSpider
+    cd ParamSpider
+    pip3 install -r requirements.txt
+    cd /tmp 
+    sudo mv /tmp/ParamSpider /opt
+}
 
-# massdns
-cd /tmp
-git clone https://github.com/blechschmidt/massdns
-cd massdns
-make
-sudo cp bin/massdns /opt
+git_i(){
+    cd /tmp
+    wget "https://github.com/tillson/git-hound/releases/download/v1.3/git-hound_1.3_Linux_x86_64.tar.gz"
+    tar -xvf git-hound_1.3_Linux_x86_64.tar.gz
+    cd /tmp
+}
 
-# nuclei
-GO111MODULE=on go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
-nuclei -update-templates
+fav_i(){
+    
+    if [[ $# -ne 0]]
+    then
+        sudo rm -rf /opt/FavFreak
+    fi
 
-# naabu
-GO111MODULE=on go get -u -v github.com/projectdiscovery/naabu/v2/cmd/naabu
+    git clone https://github.com/devanshbatham/FavFreak
+    python3 -m pip install mmh3
+    sudo mv FavFreak /opt
+    cd /tmp
+}
 
-# waybackurls
-go get github.com/tomnomnom/waybackurls
+secret_i(){
+    if [[ $# -ne 0]]
+    then
+        sudo rm -rf /opt/secretfinder
+    fi
 
-# gao
-GO111MODULE=on go get -u -v github.com/lc/gau
+    git clone https://github.com/m4ll0k/SecretFinder.git secretfinder
+    cd secretfinder
+    python -m pip install -r requirements.txt or pip install -r requirements.txt
+    cd /tmp
+    sudo mv /tmp/secretfinder /opt
+}
 
-# paramspider
-git clone https://github.com/devanshbatham/ParamSpider
-cd ParamSpider
-pip3 install -r requirements.txt
-cd /tmp 
-sudo mv /tmp/ParamSpider /opt
-echo "export PATH=$PATH:/opt/ParamSpider" >> ~/.bashrc
-echo "export PATH=$PATH:/opt/ParamSpider" >> ~/.zshrc
+dirsearch_i(){
+    if [[ $# -ne 0]]
+    then
+        sudo rm -rf /opt/dirsearch
+    fi
 
-# delfox
-go get -u github.com/hahwul/dalfox
+    git clone https://github.com/maurosoria/dirsearch.git
+    pip3 install -r dirsearch/requirements.txt
+    sudo mv dirsearch /opt
+    chmod +x /opt/dirsearch/dirsearch.py
+}
 
-# githound
-cd /tmp
-wget "https://github.com/tillson/git-hound/releases/download/v1.3/git-hound_1.3_Linux_x86_64.tar.gz"
-tar -xvf git-hound_1.3_Linux_x86_64.tar.gz
-cd /tmp
+gf_i(){
+    go get -u github.com/tomnomnom/gf
+    echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
+    go get -u github.com/tomnomnom/waybackurls
+    mkdir ~/.gf
+    cp -r $GOPATH/src/github.com/tomnomnom/gf/examples ~/.gf
+    git clone https://github.com/1ndianl33t/Gf-Patterns
+    mv Gf-Patterns/*.json ~/.gf
+    rm -rf Gf-Patterns
 
-# favfreak
-git clone https://github.com/devanshbatham/FavFreak
-python3 -m pip install mmh3
-sudo mv FavFreak /opt
-cd /tmp
+    echo "{
+        "flags": "-ivE",
+        "patterns": [
 
-# jsscan
-# secretfinder
-git clone https://github.com/m4ll0k/SecretFinder.git secretfinder
-cd secretfinder
-python -m pip install -r requirements.txt or pip install -r requirements.txt
-cd /tmp
-sudo mv /tmp/secretfinder /opt
-echo "export PATH=$PATH:/opt/secretfinder" >> ~/.bashrc
-echo "export PATH=$PATH:/opt/secretfinder" >> ~/.zshrc
+            ".pdf",
+            ".jpg",
+            ".gif",
+            ".mp3",
+            ".mp4",
+            ".jpeg",
+            ".img",
+            ".ttf",
+            ".ico",
+            ".png",
+            ".css"
+    ]
+    }
+    " > ~/.gf/files.txt
+}
+
+s3_i(){
+    if [[ $# -ne 0]]
+    then
+        sudo rm -rf /opt/S3Scanner
+    fi
+
+    cd /tmp
+    git clone https://github.com/sa7mon/S3Scanner
+    cd S3Scanner
+    pip3 install -r requirements.txt
+    cd ..
+    sudo mv S3Scanner /opt
+    go get -u github.com/hahwul/s3reverse
+}
+
+eye_i(){
+    git clone https://github.com/FortyNorthSecurity/EyeWitness
+    sudo mv EyeWitness /opt
+    cd /opt/EyeWitness/setup
+    sudo bash setup.sh
+}
+
+aqua_i(){
+
+    if [[ $# -ne 0]]
+    then
+       echo "Manually update aquatone"
+    else
+        cd /tmp
+        wget "https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip"
+        unzip *.zip
+        rm -rf *.zip
+        sudo mv aquatone /opt
+    fi
+}
+
+gob_i(){
+    cd /tmp
+    git clone https://github.com/OJ/gobuster
+    cd gobuster
+    make
+    sudo mv gobuster /opt
+    cd ..
+    rm -rf gobuster
+}
+
+wordlist_i(){
 
 
-# dirsearch
-git clone https://github.com/maurosoria/dirsearch.git
-pip3 install -r dirsearch/requirements.txt
-sudo mv dirsearch /opt
-chmod +x /opt/dirsearch/dirsearch.py
-echo "export PATH=$PATH:/opt/dirsearch" >> ~/.bashrc
-echo "export PATH=$PATH:/opt/dirsearch" >> ~/.zshrc
+    if [[ ! -d /opt/Payloadallthings ]]
+    then
+        cd /opt
+        sudo git clone https://github.com/swisskyrepo/PayloadsAllTheThings
+    fi
 
-# s3reverse
-go get -u github.com/hahwul/s3reverse
+    if [[ ! -d /opt/SecLists-master ]]
+    then
+        cd /opt
+        sudo wget "https://github.com/danielmiessler/SecLists/archive/master.zip"
+        sudo unzip master.zip
+        sudo rm -rf masater.zip
 
-# httpx
-GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
+    fi
+    
+    cd /opt
+    sudo wget https://github.com/janmasarik/resolvers/raw/master/resolvers.txt
 
-# gf
-go get -u github.com/tomnomnom/gf
-echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
+}
 
-# gf-patterns
-go get -u github.com/tomnomnom/waybackurls
-mkdir ~/.gf
-cp -r $GOPATH/src/github.com/tomnomnom/gf/examples ~/.gf
-git clone https://github.com/1ndianl33t/Gf-Patterns
-mv Gf-Patterns/*.json ~/.gf
-rm -rf Gf-Patterns
+tom_i(){
+    cd /tmp
+    git clone https://github.com/tomnomnom/hacks
+    cd hacks/kxss
+    go build
+    sudo mv kxss /opt
+    cd /tmp 
+    sudo mv hacks /opt
+}
 
-# whatweb
-sudo apt install whatweb
 
-# s3scanner
-cd /tmp
-git clone https://github.com/sa7mon/S3Scanner
-cd S3Scanner
-pip3 install -r requirements.txt
-cd ..
-sudo mv S3Scanner /opt
-#------------------------------#
-# Eyewitness
-git clone https://github.com/FortyNorthSecurity/EyeWitness
-sudo mv EyeWitness /opt
-cd /opt/EyeWitness/setup
-sudo bash setup.sh
+if [[ $# -ne 0 ]]
+then
+    echo "UPDATING..."
+    base_i
+    go_i
+    gotools_i
+    masscan_i
+    amass_i
+    massdns_i
+    gf_i
+    gob_i
+    wordlist_i
+    tom_i
+    #git_i
+    #eye_i
+    
+    aqua_i 1
+    s3_i 1
+    #dirsearch_i 1
+    secret_i 1
+    fav_i 1
+    param_i 1
+else
+    echo "INSTALLING..."
+    base_i
+    go_i
+    gotools_i
+    masscan_i
+    amass_i
+    massdns_i
+    gf_i
+    gob_i
+    wordlist_i
+    tom_i
+    #git_i
+    #eye_i
 
-# Aquatone
-cd /tmp
-wget "https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip"
-unzip *.zip
-sudo mv aquatone /opt
-
-# Gobuster
-git clone https://github.com/OJ/gobuster
-cd gobuster
-make
-sudo mv gobuster /opt
-cd ..
-rm -rf gobuster
-
-## Wordlists and Payloads
-# payloadallthings
-cd /opt
-sudo git clone https://github.com/swisskyrepo/PayloadsAllTheThings
-
-# seclists
-cd /opt
-#sudo wget "https://github.com/danielmiessler/SecLists/archive/master.zip"
-#sudo unzip master.zip
-cd /tmp
-
-cd /opt
-sudo wget https://github.com/janmasarik/resolvers/raw/master/resolvers.txt
+    aqua_i 
+    s3_i 
+    #dirsearch_i 
+    secret_i 
+    fav_i 
+    param_i
+fi
--- morty.sh
+++ morty.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
 #-----------------------------------------------#
+date=$(date +%d_%b_%Y)
 resolvers=/opt/resolvers.txt
-smallwordlist=/opt/SecLists-master/Discovery/DNS/deepmagic.com-prefixes-top500.txt
-wordlist=opt/SecLists-master/Discovery/DNS/dns-Jhaddix.txt
-fuzzword=/opt/SecLists-master/Discovery/Web-Content/raft-large-words.txt
 templatefile=/opt/nuclei.txt
 eye=/opt/EyeWitness/Python/EyeWitness.py
-date=$(date +%d_%b_%Y)
-
-
+wordlist=opt/SecLists-master/Discovery/DNS/dns-Jhaddix.txt
+fuzzword=/opt/SecLists-master/Discovery/Web-Content/raft-large-words.txt
+smallwordlist=/opt/SecLists-master/Discovery/DNS/deepmagic.com-prefixes-top500.txt
 #-----------------------------------------------#
+
+subdomain_brute(){
+  echo "#------------------------------------#"
+  echo "RUNNING SUBDOMAIN-BRUTE"
+  echo "#------------------------------------#"
+
+  for i in $(cat $1)
+  do
+    shuffledns -massdns /opt/massdns -d $i -w $wordlist -r $resolvers -o ${i}_brute.txt
+  done
+
+  cat *brute.txt | sort | uniq > brute_force_domain.txt
+  rm -rf *brute.txt
+  
+}
 
 subdomain_scan() {
     echo "#------------------------------------#"
@@ -20,26 +33,8 @@ subdomain_scan() {
     #amass enum -active -df $1 -o amass.txt -timeout 1 -max-dns-queries 150 -noresolvrate 
     subfinder -silent -dL $1 -timeout 5 -t 100 -nW -nC -o subfinder.txt &
     shuffledns -massdns /opt/massdns -list $1 -nC -r $resolvers -silent -w $wordlist -o shuffle.txt &
-
     wait
-    
-    echo "#------------------------------------#"
-    echo "RUNNING BRUTE SCAN"
-    echo "#------------------------------------#"
-    x=0
-    for i in $(cat $1)
-    do
-      shuffledns -massdns /opt/massdns -d $i -nC -silent -w $wordlist -r $resolvers -o $i_brute.txt &
-      if [[ $x -eq 10 ]]
-      then
-        wait
-      else
-        ((x=x+1))
-      fi
-    done
-
-    wait
-    cat *brute.txt amass.txt subfinder.txt shuffle.txt 2>/dev/null | sort | uniq >> temp.txt
+    cat *brute.txt amass.txt subfinder.txt shuffle.txt brute_force_domain.txt 2>/dev/null | sort | uniq >> temp.txt
     rm -rf *brute.txt amass.txt subfinder.txt shuffle.txt
 
 }
@@ -49,8 +44,14 @@ third_level() {
     echo "RUNNING THIRD LEVEL SUBDOMAIN SCAN"
     echo "#------------------------------------#"
 
-    shuffledns -massdns /opt/massdns -list temp.txt -nC -silent -w $smallwordlist -r $resolvers -o third_temp.txt  2>/dev/null
-    cat third_temp.txt temp.txt | sort | uniq | tac > subdomains.txt
+    for i in $(cat subdomains.txt)
+    do
+      shuffledns -massdns /opt/massdns -d $i -r $resolvers -o $i_third.txt 
+    done
+
+    cat *third.txt subdomains.txt | sort | uniq > temp.txt
+    mv temp.txt subdomains.txt
+
     if [[ $# -eq 1 ]]
     then
       comm -23 <(sort subdomains.txt) <(sort $1 ) | tac > mm.txt
@@ -81,23 +82,18 @@ url_extract() {
     cat raw_urls.txt | sort | uniq | gf files > urls.txt
     cat urls.txt | httpx -no-color -silent -title -status-code -content-length -fc 404 -o httpx.txt  2>/dev/null
     cat httpx.txt | awk '{print $1}'| sort | uniq > urls.txt
-    touch URL
-
-}
-
-param_discover(){
-    echo "#------------------------------------#"
-    echo "PARAMETER DISCOVERY ( $1 )"
-    echo "#------------------------------------#"
     python3 /opt/ParamSpider/paramspider.py --level high -d $i -o $(pwd)/paramspider.txt  2>/dev/null >/dev/null
-    touch PARAM
+    cat paramspider.txt >> urls.txt
+    cat urls.txt | sed 's/=.*//' > temp_url.txt
+    mv temp_url.txt urls.txt
+    touch URL
 }
 
 dal_fox(){
     echo "#------------------------------------#"
     echo "XSS SCAN ( $1 )"
     echo "#------------------------------------#"
-    cat urls.txt paramspider.txt | sort | uniq | /opt/kxss | awk '{print $NF}' | gf files | sed 's/=.*/=/' | dalfox pipe -w 1000 --silence -o $(pwd)/dalfox.txt
+    cat urls.txt  | sort | uniq | /opt/kxss | awk '{print $NF}' | gf files | sed 's/=.*/=/' | dalfox pipe -w 1000  -o $(pwd)/dalfox.txt
     touch DALFOX
 }
 
@@ -121,7 +117,7 @@ dirfuzz(){
     echo "#------------------------------------#"
     echo "DIR FUZZING ( $1 )"
     echo "#------------------------------------#"
-    /opt/gobuster dir -u $1 -w $fuzzword -o $1_gobuster.txt -q -t 100 
+    /opt/gobuster dir -u $1 -w $fuzzword -o $1_gobuster.txt -q -t 100  2>/dev/null
     touch FUZZ
 }
 
@@ -129,16 +125,24 @@ port_scan(){
     echo "#------------------------------------#"
     echo "PORT SCAN "
     echo "#------------------------------------#"
-    #sudo $naabu -iL ip.txt -p - -nC -o portscan.txt -nmap
+ 
     sudo masscan -iL ip.txt -p 1-65535 -oL portscan.txt --rate=1000 -Pn 2>/dev/null
     cat portscan.txt  | awk '{print $3}' | sort | uniq | tr '\n' ',' | sed 's/.$//' > open_ports.txt
-    sudo nmap -sCV -oN nmap_connect_scan.txt -Pn -p $(cat open_ports.txt) -iL ip.txt
+}
+
+nmap_scan(){
+  echo "#------------------------------------#"
+  echo "PORT SCAN "
+  echo "#------------------------------------#"
+
+  sudo nmap -sCV -oN nmap_connect_scan.txt -Pn -p $(cat open_ports.txt) -iL ip.txt
 }
 
 s3_scan(){
     echo "#------------------------------------#"
     echo "S3 SCAN ( $1 )"
     echo "#------------------------------------#"
+
     python3 /opt/S3Scanner/s3scanner.py -l subdomains.txt -o buckets.txt 2>/dev/null
     touch S3
 }
@@ -150,7 +154,7 @@ pattern_search(){
     mkdir pattern
     for i in $(cat /opt/gf.txt)
     do
-      cat urls.txt paramspider.txt 2>/dev/null | gf $i > pattern/${i}.txt
+      cat urls.txt  2>/dev/null | gf $i > pattern/${i}.txt
     done
     touch PATTERN
 }
@@ -175,6 +179,7 @@ secret_find(){
 }
 
 cleanup(){
+  rm -rf gecko*
   find . -empty > DELETED_EMPTYFILES.txt
   find . -empty -delete
 }
@@ -188,6 +193,7 @@ main(){
 
    if [[ ! -f subdomains.txt ]]
    then
+    subdomain_brute $1
     subdomain_scan $1
 
     if [[ $# -eq 2  ]]
@@ -196,17 +202,16 @@ main(){
     else
       third_level
     fi
-
    fi
 
    if [[ ! -f ip.txt ]]
    then
-    sub_to_ip
+    sub_to_ip &
    fi
 
    if [[ ! -f buckets.txt ]]
    then
-    s3_scan $1
+    s3_scan $1 &
    fi
 
    if [[ ! -f SCREEN ]]
@@ -229,11 +234,6 @@ main(){
           url_extract $i 
         fi
 
-        if [[ ! -f PARAM ]]
-        then
-          param_discover $i
-        fi
-
         if [[ ! -f DALFOX ]]
         then
          dal_fox $i  &
@@ -244,10 +244,10 @@ main(){
          template_scan $i & 
         fi
 
-        if [[ ! -f FAV ]]
-        then
-         favicon_scan $i & 
-        fi
+        # if [[ ! -f FAV ]]
+        # then
+        #  favicon_scan $i & 
+        # fi
 
         if [[ ! -f FUZZ ]]
         then
@@ -274,32 +274,36 @@ main(){
     then
       port_scan
     fi
-    rm -rf gecko* SCREEN S3
+
+    if [[ ! -f nmap_connect_scan.txt ]]
+    then
+      nmap_scan
+    fi
+
     cleanup
+    echo "Finished on $date" >> COMPLETED
     cd ..
-    zip -r $1.zip Project_$1
+    zip -r $1.zip Project_$1 && rm -rf Project_$1 $1 $2
 }
 
 if [[ $# -lt 1 ]]
 then
-  echo "Usage ./morty.sh <targetfile> <outofscopefile>"
+  echo -e "Usage ./morty.sh <targetfile> <outofscopefile>\n\n"
 else
   if [[ ! -d "Project_$1" ]]
   then
     mkdir "Project_$1"
   fi
 
+  # Setting up environment
   cd "Project_$1" 2> /dev/null
   cp ../$1 . 2> /dev/null
   cp ../$2 . 2>/dev/null
-
   nuclei -update-templates -silent
   find ~/nuclei-templates -type f -not -path '*/\.*' | grep .yaml > /tmp/nuclei.txt
   sudo cp /tmp/nuclei.txt /opt
-
   gf -list > /tmp/gf.txt
   sudo mv /tmp/gf.txt /opt
-
   clear
 
   if [[ $# -eq 1 ]]
@@ -308,4 +312,5 @@ else
   else
     main $1 $2
   fi
+
 fi
